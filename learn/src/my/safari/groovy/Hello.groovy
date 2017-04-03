96928db7be9fd7b708a2928c465a7ed65ef6bdce
tdongsi <dongsi.tuecuong@gmail.com>
2017-04-03 10:54:56 -0700

    Safari: Some exercises.

--- learn/src/my/safari/groovy/Hello.groovy
+++ learn/src/my/safari/groovy/Hello.groovy
@@ -5,3 +5,16 @@ package my.safari.groovy
  */
 
 println "Hello World"
+println InetAddress.localHost
+
+int x = 3
+int y = 4
+assert 7 == x + y
+// assert 7 == x + y + 1
+
+def a = 1
+println a.class.name
+println 9876432198764321.class.name
+
+def b = 0.2
+println b.class.name
