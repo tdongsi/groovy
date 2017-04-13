397eefe022bb4d7b725f77d9ebdc3535ed330090
tdongsi <dongsi.tuecuong@gmail.com>
2017-04-12 22:28:39 -0700

    Safari: Operator overloading for String.

--- learn/src/my/safari/groovy/Hello.groovy
+++ learn/src/my/safari/groovy/Hello.groovy
@@ -18,3 +18,26 @@ println 9876432198764321.class.name
 
 def b = 0.2
 println b.class.name
+println ''
+
+/*
+ * Strings in Groovy
+ */
+
+def s = 'This is a string'
+println s.reverse()
+println s.class.name
+
+/*
+ * Operator overloading for String
+ * http://docs.groovy-lang.org/latest/html/groovy-jdk/java/lang/CharSequence.html
+ */
+println s + ' and more' // plus()
+println s - 'is' // minus()
+println 'abc' * 3 // multiply()
+println s[-1] // getAt()
+println s[0..3]
+
+s = "This is a string ${1+1}"
+println s
+println s.class.name
