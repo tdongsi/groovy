b1b0766d1bb95f05007c541328b09fb59e6fda01
tdongsi <dongsi.tuecuong@gmail.com>
2017-04-13 22:44:43 -0700

    Safari: More demo with REST service.

--- learn/src/my/safari/groovy/Geocode.groovy
+++ learn/src/my/safari/groovy/Geocode.groovy
@@ -1,5 +1,7 @@
 package my.safari.groovy
 
+import groovy.json.JsonSlurper
+
 /**
  * Created by tdongsi on 4/13/17.
  * Demo from Safari: Groovy Programming Fundamentals
@@ -14,11 +16,18 @@ String encoded = address.collect {
 
 String queryString = "address=$encoded"
 String url = "$XML_BASE$queryString"
-println url.toURL().text
+// println url.toURL().text
 
 def root = new XmlSlurper().parse(url)
 def loc = root.result[0].geometry.location // Navigate DOM using dot
 println "Coordinates: ${loc.lat}, ${loc.lng}"
 
 
-String JSON_BASE = 'http://api.icndb.com/jokes/random'
+String JSON_BASE = 'http://api.icndb.com/jokes/random?'
+// Only the POTUS can claim such hyperbole.
+def params = [limitTo:'[nerdy]', firstName: 'Donald', lastName: 'Trump']
+def qs = params.collect {k,v -> "$k=$v"}.join('&')
+String jsonTxt = "$JSON_BASE$qs".toURL().text
+// println jsonTxt
+def json = new JsonSlurper().parseText(jsonTxt)
+println json.value.joke
