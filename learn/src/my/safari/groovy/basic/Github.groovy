2564023493623626521c3350230f8c2d41d287ad
tdongsi <dongsi.tuecuong@gmail.com>
2017-04-18 11:37:19 -0700

    Set credentials as environment variables. First attempt to query Github API.

--- learn/src/my/safari/groovy/basic/Github.groovy
+++ learn/src/my/safari/groovy/basic/Github.groovy
@@ -4,5 +4,13 @@ package my.safari.groovy.basic
  * Created by tdongsi on 4/18/17.
  */
 
-def env = System.getenv('GITHUB_USERNAME')
-println env
+String username = System.getenv('GITHUB_USERNAME')
+String password = System.getenv('GITHUB_PASSWORD')
+
+String GITHUB_API = 'https://api.github.com/repos/'
+String repo = 'groovy'
+String PR_ID = '1' // Pull request ID
+
+String url = "${GITHUB_API}/${username}/${repo}/pulls/${PR_ID}"
+println url
+println url.toURL().getText(requestProperties: ['Authorization': "token ${password}"])
