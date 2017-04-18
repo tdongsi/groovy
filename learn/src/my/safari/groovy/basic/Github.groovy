46fccceb11da217060b85e8bce20abceab01d17e
tdongsi <dongsi.tuecuong@gmail.com>
2017-04-18 11:53:26 -0700

    Fix bug. Add equivalent `curl` command.

--- learn/src/my/safari/groovy/basic/Github.groovy
+++ learn/src/my/safari/groovy/basic/Github.groovy
@@ -1,5 +1,7 @@
 package my.safari.groovy.basic
 
+import groovy.json.JsonSlurper
+
 /**
  * Created by tdongsi on 4/18/17.
  */
@@ -7,10 +9,21 @@ package my.safari.groovy.basic
 String username = System.getenv('GITHUB_USERNAME')
 String password = System.getenv('GITHUB_PASSWORD')
 
-String GITHUB_API = 'https://api.github.com/repos/'
+String GITHUB_API = 'https://api.github.com/repos'
 String repo = 'groovy'
 String PR_ID = '1' // Pull request ID
 
 String url = "${GITHUB_API}/${username}/${repo}/pulls/${PR_ID}"
-println url
-println url.toURL().getText(requestProperties: ['Authorization': "token ${password}"])
+println "Querying ${url}"
+def text = url.toURL().getText(requestProperties: ['Authorization': "token ${password}"])
+def json = new JsonSlurper().parseText(text)
+def bodyText = json.body
+
+// Check if Pull Request body has certain text
+if ( bodyText.find('Safari') ) {
+    println 'Found Safari user'
+}
+
+String cmd = "curl -s -H \"Authorization: token ${password}\" ${url}"
+println "Equivalent shell command: $cmd"
+println cmd.execute().text
