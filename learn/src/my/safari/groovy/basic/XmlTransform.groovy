c0b31782a93e60b7aea029628ef98d68e5c7e16f
tdongsi <dongsi.tuecuong@gmail.com>
2017-06-14 18:57:26 -0700

    Xml Transform.

---
+++ learn/src/my/safari/groovy/basic/XmlTransform.groovy
@@ -0,0 +1,30 @@
+package my.safari.groovy.basic
+
+import groovy.xml.StreamingMarkupBuilder
+import groovy.xml.XmlUtil
+
+/**
+ * Created by tdongsi on 6/14/17.
+ */
+
+def MYFILE = '/Users/tdongsi/Matrix/Aqueduct/settings.xml'
+def inFile = new File( MYFILE )
+def outFile = new File( 'temp.xml')
+def xml = new XmlSlurper(false, false).parse(inFile)
+
+println xml.servers.server[0].username
+println xml.servers.server[0].password
+
+/*
+xml.servers.server[0].username = 'user101'
+xml.servers.server[0].password = 'password123'
+*/
+
+xml.servers.server.each { node ->
+    node.username = 'user101'
+    node.password = 'password123'
+}
+
+def outBuilder = new StreamingMarkupBuilder()
+def outWriter = outFile.newWriter()
+XmlUtil.serialize( outBuilder.bind{ mkp.yield xml }, outWriter )
