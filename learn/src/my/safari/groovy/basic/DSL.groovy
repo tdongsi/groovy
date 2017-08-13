59c23e5eab7213c5da66cd6e458f9a186d438fd0
tdongsi <dongsi.tuecuong@gmail.com>
2017-08-13 02:02:29 -0700

    DSL: Groovy standard implementation.

--- learn/src/my/safari/groovy/basic/DSL.groovy
+++ learn/src/my/safari/groovy/basic/DSL.groovy
@@ -21,6 +21,9 @@ class JavaDsl {
     // A more advanced DSL
     void withEnv(String var) {
         println "Using: $var"
+    }
+
+    void execute() {
         println "Executing ..."
     }
 
@@ -28,11 +31,13 @@ class JavaDsl {
 
 println "1) Standard Java implementation"
 JavaDsl javaDsl = new JavaDsl()
+javaDsl.withEnv("PATH=/usr/bin")
 javaDsl.echo("Starting pipeline")
 javaDsl.sh("ls .")
 javaDsl.error("Error here")
-javaDsl.withEnv("PATH=/usr/bin")
+javaDsl.execute()
 println ""
+
 
 /**
  * Java code with Builder pattern
@@ -58,16 +63,59 @@ class JavaBuilderDsl {
     // A more advanced DSL
     JavaBuilderDsl withEnv(String var) {
         println "Using: $var"
-        println "Executing ..."
         return this
+    }
+
+    void execute() {
+        println "Executing ..."
     }
 }
 
-println "2) Builder Java implementation"
+println "2) Fluent Java implementation (Builder)"
 JavaBuilderDsl builderDsl = new JavaBuilderDsl()
-builderDsl.echo("Starting pipeline")
+builderDsl.withEnv("PATH=/usr/bin")
+        .echo("Starting pipeline")
         .sh("ls .")
         .error("Error here")
-        .withEnv("PATH=/usr/bin")
+        .execute()
 println ""
 
+
+/**
+ * Groovy code with standard implementation
+ * Try to simulate some kind of DSL like Pipeline steps in Jenkins
+ */
+class GroovyDsl {
+
+    def echo(String message) {
+        println "Echo: $message"
+    }
+
+    def sh(String script) {
+        println "Executing: $script"
+    }
+
+    def error(String message) {
+        println "Error here: $message"
+    }
+
+    // A more advanced DSL
+    def withEnv(String var) {
+        println "Using: $var"
+    }
+
+    static void execute(closure) {
+        GroovyDsl body = new GroovyDsl()
+        closure(body)
+        println "Executing ..."
+    }
+
+}
+
+println "3) Standard Groovy implementation"
+GroovyDsl.execute { it ->
+    it.withEnv("PATH=/usr/bin")
+    it.echo("Starting pipeline")
+    it.sh("ls .")
+    it.error("Error here")
+}
